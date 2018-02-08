0125e2f54bdbcd06e8990fe1a3997759df189f3f
Mishen <mishen888@yandex.ru>
2018-02-09 00:18:48 +0300

    Add project files.

---
+++ Sequence of Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sequence_of_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
