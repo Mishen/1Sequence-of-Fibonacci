d06133d5921c19412532c64f2aa7f73d73d251dc
Mishen <mishen888@yandex.ru>
2018-02-12 20:42:33 +0300

    1st

--- Sequence of Fibonacci/Program.cs
+++ Sequence of Fibonacci/Program.cs
@@ -6,7 +6,24 @@ namespace Sequence_of_Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+            double a = 0, b = 1, c ;
+            //Console.WriteLine(a);
+            for (int j = 1; j <= 100; j ++)
+            {
+                if (j > 1)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            
         }
     }
 }
